0225e4a87171cb9025714544441c532de787d381
Foxtrot007-ai <kaziuk28@gmail.com>
2024-05-04 17:52:13 +0200

    EditMode Test For Button Script Hierarchy

---
+++ ArduinoNodeBasedIDE/Assets/Tests/EditMode/ut/UI & Scene Classes/Buttons Classes/InputButtonTests.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Backend.API;
+using Backend.API.DTO;
+using NUnit.Framework;
+using TMPro;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace ut.UIClasses
+{
+    public class InputButtonTests
+    {
+
+        [Test]
+        public void InputButtonScriptSetVariableTest()
+        {
+            //given
+            //Creating variable
+            IVariableManage node = new VariableFake();
+            node.Change(new VariableManageDto
+            {
+                VariableName = "test Function",
+                Type = new MyTypeFake
+                {
+                    EType = Backend.Type.EType.Int,
+                    TypeName = "Int"
+                }
+            });
+
+            //creating fake button object with his fields
+            GameObject button = new GameObject();
+            InputButtonScript script = button.AddComponent<InputButtonScript>();
+
+            GameObject nameField = new GameObject();
+            nameField.AddComponent<TextMeshProUGUI>();
+
+            GameObject typeField = new GameObject();
+            typeField.AddComponent<TextMeshProUGUI>();
+
+            script.text = nameField;
+            script.typeField = typeField;
+            //when
+
+            script.SetNodeBlock(node);
+
+            //then
+            Assert.AreEqual(nameField.GetComponent<TMP_Text>().text, node.Name);
+            Assert.AreEqual(typeField.GetComponent<TMP_Text>().text, node.Type.TypeName);
+        }
+    }
+}
+
---
+++ ArduinoNodeBasedIDE/Assets/Tests/EditMode/ut/UI & Scene Classes/Buttons Classes/ReferenceButtonTests.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Backend.API;
+using Backend.API.DTO;
+using NUnit.Framework;
+using TMPro;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace ut.UIClasses
+{
+    public class ReferenceButtonTests
+    {
+        [Test]
+        public void ReferenceButtonScriptSetFunctionTest()
+        {
+            //given
+            IFunctionManage node = new FunctionFake();
+            node.Change(new FunctionManageDto
+            {
+                FunctionName = "test Function",
+                OutputType = new MyTypeFake 
+                { 
+                    EType = Backend.Type.EType.Void, 
+                    TypeName = "Void"
+                }
+            });
+
+            GameObject button = new GameObject();
+            ReferenceButtonScript script = button.AddComponent<ReferenceButtonScript>();
+
+            GameObject text = new GameObject();
+            text.AddComponent<TextMeshProUGUI>();
+
+            script.text = text;
+            //when
+
+            script.SetNodeBlock(node);
+
+            //then
+            Assert.AreEqual(text.GetComponent<TMP_Text>().text, node.Name);
+        }
+        [Test]
+        public void ReferenceButtonScriptSetVariableTest()
+        {
+            //given
+            IVariableManage node = new VariableFake();
+            node.Change(new VariableManageDto
+            {
+                VariableName = "test Function",
+                Type = new MyTypeFake
+                {
+                    EType = Backend.Type.EType.Int,
+                    TypeName = "Int"
+                }
+            });
+
+            GameObject button = new GameObject();
+            ReferenceButtonScript script = button.AddComponent<ReferenceButtonScript>();
+
+            GameObject text = new GameObject();
+            text.AddComponent<TextMeshProUGUI>();
+
+            script.text = text;
+            //when
+
+            script.SetNodeBlock(node);
+
+            //then
+            Assert.AreEqual(text.GetComponent<TMP_Text>().text, node.Name);
+        }
+    }
+}
+
