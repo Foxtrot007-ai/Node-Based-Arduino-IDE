931bf59bd4752028d1e39b39cf11f344face44f9
Jakub Mikolajczyk <Mikolajczyk.Jakub@outlook.com>
2024-07-27 20:08:08 +0200

    NF Create whileNode

---
+++ ArduinoNodeBasedIDE/Assets/Scripts/Backend/Node/BuildIn/WhileNode.cs
@@ -0,0 +1,31 @@
+using Backend.Connection;
+using Backend.Connection.MyType;
+using Backend.Template;
+using Backend.Type;
+
+namespace Backend.Node.BuildIn
+{
+    public class WhileNode : BuildInNode
+    {
+        private PrimitiveInOut _predicate;
+        private FlowInOut _loop;
+        
+        public WhileNode(BuildInTemplate buildInTemplate) : base(buildInTemplate)
+        {
+            _predicate = new PrimitiveInOut(this, InOutSide.Input, new PrimitiveType(EType.Bool));
+            AddInputs(_prevNode, _predicate);
+ 
+            _loop = new FlowInOut(this, InOutSide.Output, "body");
+            AddOutputs(_nextNode, _loop);
+        }
+
+        protected override void MakeCode(CodeManager codeManager)
+        {
+            codeManager.AddLine($"while ({ConnectedToCodeParam(codeManager, _predicate)})");
+
+            var loopCopy = new CodeManager(codeManager);
+            ConnectedToCode(loopCopy, _loop);
+            codeManager.AddLines(loopCopy.CodeLines);
+        }
+    }
+}
